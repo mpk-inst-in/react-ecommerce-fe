6059e49a4715b2099bb749f48d16e47bdd609e43
Katti, Prasad <mpk.inst.in@gmail.com>
2022-05-26 10:26:52 +0530

    Initial version of the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+      <h2>Welcome to the React E-commerce App!!</h2>
     </div>
   );
 }
