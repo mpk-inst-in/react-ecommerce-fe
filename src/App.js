f82245d74dc733d75fd1d43e7f948f0aeb40f3ad
mpk.inst.in@gmail.com <mpk.inst.in@gmail.com>
2022-05-27 21:37:29 +0530

    Implement Landing Page Feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+
 import './App.css';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
-      <h2>Welcome to the React E-commerce App!!</h2>
-    </div>
+
+  const renderComponent = () => {
+
+    return (
+
+      <>
+        <LandingPage />
+      </>
+    )
+  }
+
+
+  return (
+
+
+    renderComponent()
+
   );
 }
 
